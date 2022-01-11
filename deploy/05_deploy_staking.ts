1c82977bc6e72d1e2ab2ab0a0cfd9ebc2128ea2b
liyang <ylikp@connect.ust.hk>
2022-01-11 16:59:40 +0800

    modify deploy staking scripts

--- deploy/05_deploy_staking.ts
+++ deploy/05_deploy_staking.ts
@@ -1,23 +1,35 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { parseUnits } from "ethers/lib/utils";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
+  const { deploy, get } = deployments;
 
   const { deployer } = await getNamedAccounts();
 
-  await deploy("FixedStaking", {
-    contract: "FixedStaking",
+  const DegisToken = await get("DegisToken");
+
+  const factory = await deploy("StakingPoolFactory", {
+    contract: "StakingPoolFactory",
     from: deployer,
     args: [],
     log: true,
   });
 
-  await deploy("FlexibleStaking", {
-    contract: "FlexibleStaking",
+  const blockNumber = await ethers.provider.getBlockNumber();
+
+  await deploy("CoreStakingPool", {
+    contract: "CoreStakingPool",
     from: deployer,
-    args: [],
+    args: [
+      DegisToken.address,
+      DegisToken.address,
+      factory.address,
+      blockNumber + 4000,
+      parseUnits("20"),
+      false,
+    ],
     log: true,
   });
 };
