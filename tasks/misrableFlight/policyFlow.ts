a68d3cd04d27f474a2df4d1d653bbf6785a73096
liyang <ylikp@connect.ust.hk>
2022-02-05 00:10:40 +0800

    start settle task

--- tasks/misrableFlight/policyFlow.ts
+++ tasks/misrableFlight/policyFlow.ts
@@ -2,4 +2,21 @@ import { task, types } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 // import hre from "hardhat";
 
-task("settleFDPolicy", "Settle a flight delay policy");
+import { readAddressList } from "../../scripts/contractAddress";
+
+task("settleFDPolicy", "Settle a flight delay policy").setAction(
+  async (_, hre) => {
+    const { network } = hre;
+
+    // Signers
+    const [dev_account] = await hre.ethers.getSigners();
+    console.log("The default signer is: ", dev_account.address);
+
+    const addressList = readAddressList();
+
+    // Load contract instance
+    const policyFlowAddress = addressList[network.name].PolicyFlow;
+
+    
+  }
+);
