947ca935dba7de60aa82cbfbf70a75cfadafd060
liyang <ylikp@connect.ust.hk>
2022-03-20 16:43:10 +0800

    mod comment

--- contracts/naughty-price/PriceGetter.sol
+++ contracts/naughty-price/PriceGetter.sol
@@ -45,9 +45,7 @@ contract PriceGetter is Ownable {
      * @notice Constructor function, initialize some price feed
      */
     constructor() Ownable(msg.sender) {
-        // At first, launch three kind of pools
-
-        // Uncomment below when launched on Avalanche Fuji
+        // Avalanche data feed addresses and decimals
         priceFeedInfo["AVAX"] = PriceFeedInfo(
             0x0A77230d17318075983913bC2145DB16C7366156,
             8
