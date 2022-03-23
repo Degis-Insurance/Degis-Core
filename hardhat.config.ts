4035e33bbbe1f97c81d83d78762cda8538865395
liyang <ylikp@connect.ust.hk>
2022-03-23 10:33:10 +0800

    delpoy first pre test pool

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -132,7 +132,7 @@ const config: HardhatUserConfig = {
           process.env.PHRASE_FUJI !== undefined ? process.env.PHRASE_FUJI : "",
         count: 20,
       },
-      gasPrice: 88000000000,
+      // gasPrice: 88000000000,
     },
   },
   gasReporter: {
---
+++ tasks/naughtyPrice/cli.md
@@ -0,0 +1,9 @@
+## #sd
+
+---
+
+---
+
+---
+
+npx hardhat deployNPToken --network avaxTest --name AVAX --stablecoin USDC.e --k 83.68 --namedecimals 2 --iscall 0 --round 2303 --deadline 1648094400 --settletime 1648108800
