e8ac626c888441b01d79dc79f50eb4bda5700f97
liyang <42539774+LIYANG-UST@users.noreply.github.com>
2022-06-21 23:12:31 +0800

    initial version of IDO price getter

--- contracts/naughty-price/IDOPriceGetter.sol
+++ contracts/naughty-price/IDOPriceGetter.sol
@@ -21,6 +21,8 @@
 pragma solidity ^0.8.10;
 
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { IPriceGetter } from "./interfaces/IPriceGetter.sol";
+import { IPolicyCore } from "./interfaces/IPolicyCore.sol";
 
 import { IUniswapV2Pair } from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import { FixedPoint } from "@uniswap/lib/contracts/libraries/FixedPoint.sol";
@@ -36,20 +38,128 @@ import { UniswapV2Library } from "@uniswap/v2-periphery/contracts/libraries/Unis
 
 contract IDOPriceGetter is OwnableUpgradeable {
     using FixedPoint for *;
+
+    // WAVAX address
+    address public constant WAVAX = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
+
+    // Base price getter to transfer the price into USD
+    IPriceGetter public basePriceGetter;
+
+    IPolicyCore public policyCore;
+
     struct IDOPriceInfo {
-        address pair;
+        address pair; // Pair on TraderJoe
         uint256 decimals;
-        uint256 price;
+        uint256 sampleInterval;
+        uint256 price0Average;
+        uint256 price1Average;
+        uint256 price0CumulativeLast;
+        uint256 price1CumulativeLast;
+        uint256 lastTimestamp;
         uint256 endTime;
     }
-    // Policy Token Address => IDO Info
-    mapping(address => IDOPriceInfo) priceFeeds;
+    // Policy Base Token Name => IDO Info
+    mapping(string => IDOPriceInfo) priceFeeds;
 
-    function initialize() public initializer {
+    event SamplePrice(
+        address policyToken,
+        uint256 price0Average,
+        uint256 price1Average,
+        uint256 timestamp
+    );
+
+    function initialize(address _priceGetter, address _policyCore)
+        public
+        initializer
+    {
         __Ownable_init();
+
+        basePriceGetter = IPriceGetter(_priceGetter);
+        policyCore = IPolicyCore(_policyCore);
     }
 
-    function samplePrice() external {}
+    function addIDOPair(
+        string calldata _policyToken,
+        address _pair,
+        uint256 _decimals,
+        uint256 _interval
+    ) external onlyOwner {
+        require(IUniswapV2Pair(_pair).token0 != address(0), "Non exist pair");
 
-    function getFinalPrice() external view returns (uint256 price) {}
+        IDOPriceInfo storage newFeed = priceFeeds[_policyToken];
+
+        newFeed.pair = _pair;
+        newFeed.decimals = _decimals;
+        newFeed.sampleInterval = _interval;
+    }
+
+    function samplePrice(string calldata _policyToken) external {
+        IDOPriceInfo storage priceFeed = priceFeeds[_policyToken];
+
+        (
+            uint256 price0Cumulative,
+            uint256 price1Cumulative,
+            uint32 blockTimestamp
+        ) = UniswapV2OracleLibrary.currentCumulativePrices(priceFeed.pair);
+
+        // Time between this sampling and last sampling (seconds)
+        uint32 timeElapsed = blockTimestamp - priceFeed.lastTimestamp;
+
+        require(
+            timeElapsed > priceFeed.sampleInterval,
+            "Minimum sample interval"
+        );
+
+        // Update price0Average and price1Average
+        uint256 newPrice0Average = FixedPoint.uq112x112(
+            uint224(
+                (price0Cumulative - priceFeed.price0CumulativeLast) /
+                    timeElapsed
+            )
+        );
+        uint256 newPrice1Average = FixedPoint.uq112x112(
+            uint224(
+                (price1Cumulative - priceFeed.price1CumulativeLast) /
+                    timeElapsed
+            )
+        );
+        priceFeed.price0Average = newPrice0Average;
+        priceFeed.price1Average = newPrice1Average;
+
+        // Update price0CumulativeLast and price1CumulativeLast
+        priceFeed.price0CumulativeLast = price0Cumulative;
+        priceFeed.price1CumulativeLast = price1Cumulative;
+
+        // Update lastTimestamp
+        priceFeed.lastTimestamp = blockTimestamp;
+
+        emit SamplePrice(
+            _policyToken,
+            newPrice0Average,
+            newPrice1Average,
+            blockTimestamp
+        );
+    }
+
+    function getPrice(string calldata _policyToken)
+        external
+        view
+        returns (uint256 price)
+    {
+        address pair = priceFeeds[_policyToken].pair;
+
+        uint256 priceInAVAX;
+
+        if (IUniswapV2Pair(pair).token0 == WAVAX) {
+            priceInAVAX = priceFeeds[_policyToken].price1Average;
+        } else {
+            priceInAVAX = priceFeeds[_policyToken].price0Average;
+        }
+
+        // AVAX price, 1e18 scale
+        uint256 avaxPrice = basePriceGetter.getLatestPrice("AVAX");
+
+        // This final price is also multiplied by 1e18
+        price = avaxPrice * priceInAVAX;
+    }
 }
