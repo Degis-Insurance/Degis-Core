c371137b5b4d398dd628d47de73d052d0a4e4fba
liyang <42539774+LIYANG-UST@users.noreply.github.com>
2022-07-11 01:00:36 +0800

    :bug: Fix the interface of veDEG

--- contracts/governance/interfaces/IVeDEG.sol
+++ contracts/governance/interfaces/IVeDEG.sol
@@ -4,20 +4,16 @@ pragma solidity ^0.8.10;
 import "./IVeERC20.sol";
 
 /**
- * @dev Interface of the VePtp
+ * @dev Interface of the VeDEG
  */
 interface IVeDEG is IVeERC20 {
-    function isUser(address _addr) external view returns (bool);
-
     function deposit(uint256 _amount) external;
+
+    function depositMaxTime(uint256 _amount) external;
 
     function claim() external;
 
     function withdraw(uint256 _amount) external;
-
-    function getStakedPtp(address _addr) external view returns (uint256);
-
-    function getVotes(address _account) external view returns (uint256);
 
     function lockVeDEG(address _to, uint256 _amount) external;
 
